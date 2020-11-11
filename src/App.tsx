5dc709a2565ab94c1ccde6270731be3b3ed946f9
Victor Silva <victormarques.ia@gmail.com>
2020-11-11 17:22:40 -0300

    project base

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import GlobalStyle from './globalStyles';
+import { Login, Dashboard, Index, Register} from './pages';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <GlobalStyle />
+      <Switch>
+        <Route path="/" component={Index} exact />
+        <Route path="/login" component={Login} exact />
+        <Route path="/register" component={Register} exact />
+        <Route path="/dashboard" component={Dashboard} exact />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
