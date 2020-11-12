9acda4153f2eb654c7ecb01501460c7891da6c06
Victor Silva <victormarques.ia@gmail.com>
2020-11-11 23:20:28 -0300

    update: Login page

--- src/pages/Login/Login.elements.tsx
+++ src/pages/Login/Login.elements.tsx
@@ -1,1 +1,7 @@
 import styled from 'styled-components';
+
+export const LoginButton = styled.button``;
+
+export const Input = styled.input``;
+
+export const Label = styled.label``;
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import React, { createRef } from 'react'
+import makeToast from '../../global/toaster';
+import api from '../../global/api';
 
-const Login = () => {
+import { Card, CardBody, CardHeader, InputGroup } from '../../global/globalStyles';
+
+import { Input, Label, LoginButton } from './Login.elements';
+
+const Login = (props: { history: string[]; }) => {
+  const emailRef = createRef<HTMLInputElement>();
+  const passwordRef = createRef<HTMLInputElement>();
+
+  const loginUser = () => {
+    let email;
+    let password;
+
+    if(emailRef.current && passwordRef.current) {
+      email = emailRef.current.value;
+      password = passwordRef.current.value;
+    }
+
+    api
+    .post('/user/login', {
+      email,
+      password,
+    })
+    .then((response) => {
+      makeToast('success', response.data.message);
+      localStorage.setItem('CA_Token', response.data.token);
+      props.history.push('/dashboard');
+    })
+    .catch((err) => {
+      if (
+        err &&
+        err.response &&
+        err.response.data &&
+        err.response.data.message
+      )
+        makeToast('error', err.response.data.message);
+    });
+  }
+
   return (
-    <h1>Login</h1>
+    <Card>
+      <CardHeader>Login</CardHeader>
+      <CardBody>
+        <InputGroup>
+          <Label htmlFor="email">Email</Label>
+          <Input type="email" name="email" id="email" placeholder="your@mail.com" ref={emailRef} />
+        </InputGroup>
+        <InputGroup>
+          <Label htmlFor="password">Password</Label>
+          <Input type="password" name="password" id="password" placeholder="Your Password" ref={passwordRef} />
+        </InputGroup>
+        <LoginButton onClick={loginUser}>Login</LoginButton>
+      </CardBody>
+    </Card>
   )
 }
 
