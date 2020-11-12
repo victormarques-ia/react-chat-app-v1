f1af2a82cd17e8d986eff5e22604189e1ac15a8e
Victor Silva <victormarques.ia@gmail.com>
2020-11-11 23:20:05 -0300

    update: Dashboard page

--- src/pages/Dashboard/Dashboard.elements.tsx
+++ src/pages/Dashboard/Dashboard.elements.tsx
@@ -1,1 +1,9 @@
 import styled from 'styled-components';
+
+export const Button = styled.button``;
+
+export const Input = styled.input``;
+
+export const Label = styled.label``;
+
+export const Div = styled.div``;
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Card, CardBody, CardHeader, InputGroup, ChatRooms, ChatRoom, Join } from '../../global/globalStyles'
+import { Label, Input, Button, Div } from './Dashboard.elements'
 
 const Dashboard = () => {
   return (
-    <h1>Dashboard</h1>
+    <Card>
+      <CardHeader>Chatrooms</CardHeader>
+      <CardBody>
+        <InputGroup>
+          <Label htmlFor="chatroomName">Chatroom Name</Label>
+          <Input type="text" name="chatroomName" id="chatroomName" placeholder="ChatterBox Portugal" />
+        </InputGroup>
+      </CardBody>
+      <Button>Create Chatroom</Button>
+      <ChatRooms>
+        <ChatRoom>
+          <Div>Happy Newbie</Div>
+          <Join>Join</Join>
+        </ChatRoom>
+        <ChatRoom>
+          <Div>Happy Newbie</Div>
+          <Join>Join</Join>
+        </ChatRoom>
+        <ChatRoom>
+          <Div>Happy Newbie</Div>
+          <Join>Join</Join>
+        </ChatRoom>
+      </ChatRooms>
+    </Card>
   )
 }
 
